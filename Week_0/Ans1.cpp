a4b56eedf7715851be94ab9695bc59dc0a1943f6
Utkarsh Kushwaha <54776016+UtkarshKushwaha1@users.noreply.github.com>
2021-07-22 09:35:54 +0530

    solved all

--- Week_0/Ans1.cpp
+++ Week_0/Ans1.cpp
@@ -1,39 +1,62 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 
-int main()
-{
-    int n,key,ind,f=0,c=0;
-    cout<<"Size of array:";
-    cin>>n;
+void linear_search(int *a,int n,int ele)
+{   
+    int count=0,f=0;
 
-    int a[n];
-
-    for(int i=0;i<n;i++)
-        cin>>a[i];
-
-    cout<<"\nEnter Key:";
-    cin>>key;
+    fstream output;
+    output.open("ouput.txt",ios::app);
 
     for(int i=0;i<n;i++)
-    {
-        c++;
-        if(a[i]==key)
+    {   count++;
+        if(a[i]==ele)
         {
-            ind=i;
             f=1;
             break;
         }
     }
+    if(f==1)
+    output<<"Present!\n"<<count<<" times compared!"<<endl;
 
-    if(f==0)
-    cout<<"Element not found!!";
+    else
+    output<<"Not Present!\n"<<count<<" times compared!"<<endl;
 
-    else{
-        cout<<"Element found at index "<<ind<<" !!";
-        cout<<"\nTotal numbers of comparisons: "<<c;
+    output.close();
+
+}
+
+int main()
+{
+    fstream input;
+
+    input.open("input.txt", ios::in);
+    if(!input)
+    {
+        cout<<"Invalid file!";
+        exit(1);
     }
 
-    
-    return 0;
+    int t,n,ele;
+    input>>t;
+
+   
+    while(t--)
+    {   
+        input>>n;
+
+        int a[n];
+
+        for(int i=0;i<n;i++)
+            input>>a[i];
+        
+        input>>ele;
+
+        linear_search(a,n,ele);
+   }
+   input.close();
+
+   return 0;
+
 }
