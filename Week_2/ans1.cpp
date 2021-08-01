918a8a72e877df265afe72c97f2d9a20558e4e9c
Utkarsh Kushwaha <54776016+UtkarshKushwaha1@users.noreply.github.com>
2021-08-01 12:27:39 +0530

    ans1 added

--- Week_2/ans1.cpp
+++ Week_2/ans1.cpp
@@ -1,53 +1,51 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-bool binary_search(int a[],int s,int n,int e)
-{   
-    int mid = (s+n)/2;
-    if(e==a[mid])
-        return true;
-    
-    else if(e>a[mid])
-        binary_search(a,mid+1,n,e);
-    else  if(e<a[mid])
-        binary_search(a,0,mid,e);
-    return false;
-}
+int c=0;
 
 int search(int a[],int n,int e)
-{   int c=0;
-    bool ans = binary_search(a,0,n,e);
-    if(ans)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(a[i]==e)
-                    c++;
-            }
-        }
+{  int i=0,c=0;
+   while (a[i]<=e)
+   {
+       if(a[i]==e)
+       {
+           c++;
+       }
+       i++;
+   }
+   
+   if(c>0)
+    return c;
 
-        return c;
+    return -1;
 }
 
 int main()
 {
-    int t,n;
+    int t;
     cin>>t;
 
-    while(t--)
+    while (t--)
     {
-       cin>>n;
-       int a[n];
+        int n;
+        cin>>n;
 
-       for(int i=0;i<n;i++)
-        cin>>a[i];
+        int a[n];
 
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        
         int e;
         cin>>e;
+        
+        c=search(a,n,e);
 
-        int c=search(a,n-1,e);
+        if(c>0)
         cout<<e<<"-"<<c<<endl;
-    }
 
+        else    
+            cout<<"Key not present"<<endl;
+    }
+    
     return 0;
 }
